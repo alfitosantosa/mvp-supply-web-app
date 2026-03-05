1af783a480d3863efcc98fc6481dd0806047edc9
alfitosantosa <sanskun02pg@gmail.com>
2026-03-05 07:44:42 +0700

    add api and hooks for company

--- app/types/company.ts
+++ app/types/company.ts
@@ -1,12 +1,29 @@
+// model Company {
+//   id        String    @id @default(cuid())
+//   name      String    // Contoh: PT Multi Visi Primakreasi
+//   brandName String    // Contoh: mvpsupply.id
+//   address   String    // Jl. Cikatomas II No. 18...
+//   email     String
+//   phone     String
+//   bankName  String    // BCA
+//   bankAccount String  // 001-669-6999
+//   senderName  String  // Vici Herlambang
+//   senderTitle String  // Co-Founder
+//   invoices  Invoice[]
+// }
 
-export interface companyData {
-        name: string,
-        brandName: string,
-        address: string,
-        email: string,
-        phone: string,
-        bankName: string,
-        bankAccount: string,
-        senderName: string,
-        senderTitle: string,
+export interface companyForm {
+        name: string;
+        brandName: string;
+        address: string;
+        email: string;
+        phone: string;
+        bankName: string;
+        bankAccount: string;
+        senderName: string;
+        senderTitle: string;
+}
+
+export interface companyData extends companyForm {
+    id: string;
 }
